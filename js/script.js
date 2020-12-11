8d6f5c719b9471054792f96ee9180de4e9e1b851
Stephen Ndele <stephenndele09346@gmail.com>
2020-12-11 08:21:11 +0300

    Add js files with objects

---
+++ js/script.js
@@ -0,0 +1,30 @@
+let male = {
+    Sunday: 'Kwasi',
+
+    Monday: 'Kwadwo',
+
+    Tuesday: 'Kwabena',
+
+    Wednesday: 'Kwaku',
+
+    Thursday:  'Yaw',
+
+    Friday: 'Kofi',
+
+    Saturday: 'Kwame',
+};
+let female = {
+    Sunday: 'Akosua',
+
+    Monday: 'Adwoa',
+
+    Tuesday: 'Abenaa',
+
+    Wednesday: 'Akua',
+
+    Thursday:  'Yaa',
+
+    Friday: 'Akua',
+
+    Saturday: 'Ama',
+};
